6229e4e3391a68e6f67c4ff8e16cb2de84b6c490
RubenPiro <ruben.piro.s1588@student.hogent.be>
2015-11-20 11:13:58 +0100

    debugging script

--- windows/windows_server/AsSv1/AsScript.ps1
+++ windows/windows_server/AsSv1/AsScript.ps1
@@ -11,53 +11,45 @@ $newname = "AsSv1"
 function SetIP{
     Add-windowsfeature RSAT-AD-Tools
     $ipaddress = "192.168.210.11"
-    $ipprefix = "24"
-    $ipgw = "192.168.210.1"
-    $ipdns = "192.168.210.11"
+	$ipprefix = "24"
+	$ipgw = "192.168.210.1"
+	$ipdns = "192.168.210.11"
     $IntAlias = "Ethernet"
-    New-NetIPAddress -IPAddress $ipaddress -Interfacealias $IntAlias -PrefixLength $ipprefix -DefaultGateway $ipgw
+	New-NetIPAddress -IPAddress $ipaddress -Interfacealias $IntAlias -PrefixLength $ipprefix -DefaultGateway $ipgw
 }
 
 #AD
 function InstallAD{
-    param( 
-        [Parameter(Mandatory=$true)][string]$domain
-    )
     Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
     Import-Module ADDSDeployment
-    Install-addsforest -creatednsdelegation:$false -domainMode "Assengraaf.nl" -DomainName $domain -forestmode "Assengraaf.nl" `
+    Install-addsforest -creatednsdelegation:$false -domainMode "win2012" -DomainName "Assengraaf.nl" -SafeModeAdministratorPassword (ConvertTo-SecureString  Admin123 -AsPlainText -Force ) -forestmode "win2012" `
     -NorebootOnCompletion:$false -InstallDns:$true
 }
 
 #DHCP
 function InstallDHCP{
-    Install-WindowsFeature –Name DHCP
-    Install-WindowsFeature –Name RSAT-DHCP
-    Set-DhcpServerv4Binding -BindingState $true -InterfaceAlias "AssengraafConnectie"
-    Add-DhcpServerInDC -DnsName AsSv1.Assengraaf.nl
-    Add-DhcpServerv4Scope -Name "Assengraafscope" -StartRange 192.168.210.30 -EndRange 192.168.210.230 -SubnetMask 255.255.255.0
+    Install-WindowsFeature -Name DHCP
+    Install-WindowsFeature -Name RSAT-DHCP
+    Set-DhcpServerv4Binding -BindingState $true -InterfaceAlias "Ethernet"
+    Add-DhcpServerInDC -DnsName "AsSv1.Assengraaf.nl"
+    #Add-DhcpServerv4Scope -Name "Assengraafscope" -StartRange 192.168.210.30 -EndRange 192.168.210.230 -SubnetMask 255.255.255.0
 }
 
-#Update
-function Update{
-    Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU\AUoptions" 
-}
 
 #OU
 function CreateOU{
-    New-ADOrganizationalUnit -Name AsAfdelingen -Path 'DC=Assengraaf,DC=NL' -Description"Overkoepeldende OU voor het domein Assengraaf.nl" -ProtectedFromAccidentalDeletion $False
-    New-ADOrganizationalUnit -Name Beheer -Path 'OU=AsAfdelingen,DC=Assengraaf,DC=NL' -Description"Groep voor de Beheerders" -ProtectedFromAccidentalDeletion $False #nog block inheritance
-    New-ADOrganizationalUnit -Name Directie -Path 'OU=AsAfdelingen,DC=Assengraaf,DC=NL' -Description"Groep voor de Directie" -ProtectedFromAccidentalDeletion $False
-    New-ADOrganizationalUnit -Name Verzekeringen -Path 'OU=AsAfdelingen,DC=Assengraaf,DC=NL' -Description"Groep voor de verzekeringen" -ProtectedFromAccidentalDeletion $False
-    New-ADOrganizationalUnit -Name Financieringen -Path 'OU=AsAfdelingen,DC=Assengraaf,DC=NL' -Description"Groep voor de Financieringen" -ProtectedFromAccidentalDeletion $False
-    New-ADOrganizationalUnit -Name Staf -Path 'OU=AsAfdelingen,DC=Assengraaf,DC=NL' -Description"Groep voor de Stafmedewerkers" -ProtectedFromAccidentalDeletion $False
+    New-ADOrganizationalUnit -Name AsAfdelingen -Path 'DC=Assengraaf,DC=NL' -Description "Overkoepeldende OU voor het domein Assengraaf.nl" -ProtectedFromAccidentalDeletion $False
+    New-ADOrganizationalUnit -Name Beheer -Path 'OU=AsAfdelingen,DC=Assengraaf,DC=NL' -Description "Groep voor de Beheerders" -ProtectedFromAccidentalDeletion $False #nog block inheritance
+    New-ADOrganizationalUnit -Name Directie -Path 'OU=AsAfdelingen,DC=Assengraaf,DC=NL' -Description "Groep voor de Directie" -ProtectedFromAccidentalDeletion $False
+    New-ADOrganizationalUnit -Name Verzekeringen -Path 'OU=AsAfdelingen,DC=Assengraaf,DC=NL' -Description "Groep voor de verzekeringen" -ProtectedFromAccidentalDeletion $False
+    New-ADOrganizationalUnit -Name Financieringen -Path 'OU=AsAfdelingen,DC=Assengraaf,DC=NL' -Description "Groep voor de Financieringen" -ProtectedFromAccidentalDeletion $False
+    New-ADOrganizationalUnit -Name Staf -Path 'OU=AsAfdelingen,DC=Assengraaf,DC=NL' -Description "Groep voor de Stafmedewerkers" -ProtectedFromAccidentalDeletion $False
 }
 
 #CSV Import
 function Add-Users {
-    $Users = Import-CSV -Delimiter "," -Path ".\AsCSV.csv" 
+    $Users = Import-CSV -Delimiter "," -Path "AsCSV.csv" 
 	ForEach($User in $Users) {
-        $i++
 	    $DisplayName = $User.GivenName + " " + $User.Surname
 	    $UserFirstname = $User.GivenName
 	    $FirstLetterFirstname = $UserFirstname.substring(0,1)
@@ -65,9 +57,8 @@ function Add-Users {
 	    $SurName = $User.Surname
 	    $GivenName = $User.GivenName
 	    $SAM = $FirstLetterFirstname + $UserSurname
-	    $City = $User.City
 	    $Password = ConvertTo-SecureString -AsPlainText $Pass -force
-            $OuPath = 'OU=' + $User.Department + ',DC=Assengraaf,DC=NL'
+        $OuPath = 'OU=' + $User.Department + ',DC=Assengraaf,DC=NL'
     
         New-ADUser -Name $DisplayName -SamAccountName $SAM -UserPrincipalName $SAM -DisplayName $DisplayName 
         -GivenName $GivenName -SurName $SurName -Path $OuPath -AccountPassword $Password -ChangePasswordAtLogon $true -enable $true
@@ -77,4 +68,3 @@ function Add-Users {
 function Add-FolderPerUser{
 	
 }
-
