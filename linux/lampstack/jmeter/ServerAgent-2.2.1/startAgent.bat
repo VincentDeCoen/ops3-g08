918d230914c4d84417f92548489b18dd3ad9523a
birgitcroux <birgitta.croux.u1690@student.hogent.be>
2015-12-02 16:10:51 +0100

    removed serveragent

--- linux/lampstack/jmeter/ServerAgent-2.2.1/startAgent.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-/vagrant/jmeter/ServerAgent-2.2.1/JRE/bin/java -jar %0\..\CMDRunner.jar --tool PerfMonAgent %*
--- linux/remotedb/jmeter/ServerAgent-2.2.1/startAgent.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/vagrant/jmeter/ServerAgent-2.2.1/JRE/bin/java -jar $(dirname $0)/CMDRunner.jar --tool PerfMonAgent "$@"
