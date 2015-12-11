3e0d7d92e731c32632c33d89b35b79d3d395f326
RubenPiro <ruben.piro.s1588@student.hogent.be>
2015-12-11 15:36:50 +0100

    Update basis_conf.ps1

--- windows/windows_server/AsSv1/basis_conf.ps1
+++ windows/windows_server/AsSv1/basis_conf.ps1
@@ -8,12 +8,29 @@ $newname = "AsSv1"
 function SetIP{
     Add-windowsfeature RSAT-AD-Tools
     $ipaddress = "192.168.210.11"
-	$ipprefix = "24"
-	$ipgw = "192.168.210.1"
-	$ipdns = "192.168.210.11"
+    $ipgw = "192.168.210.1"
+    $ipdns = "192.168.210.11"
+    $ippref = "24"
     $IntAlias = "Ethernet"
-	New-NetIPAddress -IPAddress $ipaddress -Interfacealias $IntAlias -PrefixLength $ipprefix -DefaultGateway $ipgw
+    
+    New-NetIPAddress -InterfaceIndex 12  -IPAddress $ipaddress -Interfacealias $IntAlias -PrefixLength $ippref -DefaultGateway $ipgw
 }
 
+#Activate the server
+#function{
+#	slmgr.vbs -ipk<productkey>
+#	slmgr.vbs -ato
+#}
+
 #Restart The Server (To complete the rename)
+function rename{
 	Restart-Computer
+}
+
+#Firewall
+function setFirewall{
+
+Set-NetFirewallProfile -DefaultInboundAction Block -DefaultOutboundAction Allow –NotifyOnListen True -AllowUnicastResponseToMulticast True –LogFileName %SystemRoot%\System32\LogFiles\Firewall\pfirewall.log
+
+}
+
