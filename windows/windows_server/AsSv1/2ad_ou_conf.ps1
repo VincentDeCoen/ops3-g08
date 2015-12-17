b3396dd3fb32222b1b040fabf3b39112daf450ff
RubenPiro <ruben.piro.s1588@student.hogent.be>
2015-12-17 01:00:52 +0100

    Update 2ad_ou_conf.ps1

--- windows/windows_server/AsSv1/2ad_ou_conf.ps1
+++ windows/windows_server/AsSv1/2ad_ou_conf.ps1
@@ -1,12 +1,25 @@
 #Installation of AD and OU's and Users with CSV-file
 function InstallAD{
 	
-	Write-Host "Server is getting AD"
-   Start-Sleep -s 2
+    Write-Host "Server is getting AD"
+    Start-Sleep -s 2
 	
-    Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
     Import-Module ADDSDeployment
-    Install-addsforest -creatednsdelegation:$false -domainMode "win2012" -DomainName "Assengraaf.nl" -SafeModeAdministratorPassword (ConvertTo-SecureString  Admin123 -AsPlainText -Force ) -forestmode "win2012" `
+    Install-windowsfeature -name AD-Domain-Services -IncludeManagementTools
+    
+    Install-ADDSForest `
+    -CreateDnsDelegation:$false `
+    -DatabasePath "C:\Windows\NTDS" `
+    -DomainMode "Win2012R2" `
+    -DomainName "Assengraaf.nl" `
+    -DomainNetbiosName "ASSENGRAAF" `
+    -ForestMode "Win2012R2" `
+    -InstallDns:$true `
+    -LogPath "C:\Windows\NTDS" `
+    -NoRebootOnCompletion:$true `
+    -SysvolPath: "C:\Windows\SYSVOL" `
+    -Force:$true `
+    -SafeModeAdministratorPassword (ConvertTo-SecureString  Admin123 -AsPlainText -Force ) -forestmode "win2012" `
     -NorebootOnCompletion:$false -InstallDns:$true
 }
 
@@ -14,7 +27,7 @@ function InstallAD{
 function CreateOU{
 	
 	Write-Host "Server is getting the ASSENGRAAF OU's "
-   Start-Sleep -s 2
+    Start-Sleep -s 2
 	
     New-ADOrganizationalUnit -Name AsAfdelingen -Path 'DC=Assengraaf,DC=NL' -Description "Overkoepeldende OU voor het domein Assengraaf.nl" -ProtectedFromAccidentalDeletion $False
     New-ADOrganizationalUnit -Name Beheer -Path 'OU=AsAfdelingen,DC=Assengraaf,DC=NL' -Description "Groep voor de Beheerders" -ProtectedFromAccidentalDeletion $False
@@ -56,7 +69,7 @@ function Add-Users {
         -GivenName $GivenName -SurName $SurName -Path $U -AccountPassword $Password -ChangePasswordAtLogon $true -enable $true -HomeDirectory "\\users\$FullName\HomeDir"
 
 	Write-Host "User $DisplayName is created"
-   Start-Sleep -s 1
+    Start-Sleep -s 1
 	
 	}
 }
