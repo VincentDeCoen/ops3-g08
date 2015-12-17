5ddc487a2d9713a275738eab2fce7a14515c86cf
RubenPiro <ruben.piro.s1588@student.hogent.be>
2015-12-17 02:33:50 +0100

    Optimalisatie DHCP

--- windows/windows_server/AsSv1/3dhcp_conf.ps1
+++ windows/windows_server/AsSv1/3dhcp_conf.ps1
@@ -1,10 +1,25 @@
 #DHCP
-function InstallDHCP{
+function DHCP{
     Install-WindowsFeature -Name DHCP
     Install-WindowsFeature -Name RSAT-DHCP
-    Set-DhcpServerv4Binding -BindingState $true -InterfaceAlias "Ethernet"
-    Add-DhcpServerInDC -DnsName "AsSv1.Assengraaf.nl"
-		Add-DhcpServerv4Scope -Name "Assengraafscope" -StartRange 192.168.210.31 -EndRange 192.168.210.130 -SubnetMask 255.255.255.0
-    Set-DhcpServerv4DnsSetting -ScopeId 192.168.210.0 -DynamicUpdates OnClientRequest -DeleteDnsRROnLeaseExpiry $true
-    Set-DhcpServerv4OptionValue -ScopeId 192.168.210.0 -DnsServer 192.168.210.11 -DnsDomain Assengraaf.nl -Router 192.168.210.11
-}
+    
+    Set-DhcpServerv4Binding -BindingState $true `
+    -InterfaceAlias "Ethernet"
+    
+    Add-DhcpServerInDC -DnsName "AsSv1.Assengraaf.nl" `
+	
+    Add-DhcpServerv4Scope -Name "Assengraafscope" `
+    -StartRange 192.168.210.81 `
+    -EndRange 192.168.210.130 `
+    -SubnetMask 255.255.255.0
+    
+    Set-DhcpServerv4DnsSetting -ScopeId 192.168.210.0 `
+    -DynamicUpdates OnClientRequest `
+    -DeleteDnsRROnLeaseExpiry $true
+    
+    Set-DhcpServerv4OptionValue -ScopeId 192.168.210.0 `
+    -DnsServer 192.168.210.10 `
+    -DnsDomain Assengraaf.nl `
+    -Router 192.168.210.10
+
+  }
